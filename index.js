6d9537372ce908faefd69b744ba4d2062c0b08b3
splean101 <101splean@gmail.com>
2023-04-01 13:24:31 +0300

    2

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const PORT = 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to our shop</h1>');
-});
+//------1------
+// app.get('/', (req, res) => {
+//   res.send('<h1>Welcome to our shop</h1>');
+// });
 
 app.get('/product', (req, res) => {
   let title = req.query?.title || 'Title';
@@ -13,4 +16,12 @@ app.get('/product', (req, res) => {
   res.send(`<h1>${title}</h1> For just <h2>${price}</h2>`);
 });
 
-app.listen(PORT, ()=>console.log('Server is running...'))
+//------2------
+app.get('/', (req, res) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(PORT, () => console.log('Server is running...'));
