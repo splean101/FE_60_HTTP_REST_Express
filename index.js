2f2567b13c860153a0da04642a6b5424fc7e1d0e
splean101 <101splean@gmail.com>
2023-04-01 14:35:33 +0300

    4

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import swaggerUi from 'swagger-ui-express';
+import { createRequire } from 'node:module';
+
+
 
 const PORT = 3000;
 const app = express();
@@ -35,5 +39,11 @@ app.get('/product', (req, res) => {
 
   res.render('index', {title, price});
 });
+
+//-----4-----
+const require = createRequire(import.meta.url);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require('./swagger.json')));
+
+
 
 app.listen(PORT, () => console.log('Server is running...'));
