5ca58f5584a99c6045ba67bef48a310591cf3665
splean101 <101splean@gmail.com>
2023-04-01 14:03:39 +0300

    3

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const app = express();
 //   res.send('<h1>Welcome to our shop</h1>');
 // });
 
-app.get('/product', (req, res) => {
-  let title = req.query?.title || 'Title';
-  const price = req.query?.price || 'Price';
-  res.send(`<h1>${title}</h1> For just <h2>${price}</h2>`);
-});
+// app.get('/product', (req, res) => {
+//   let title = req.query?.title || 'Title';
+//   const price = req.query?.price || 'Price';
+//   res.send(`<h1>${title}</h1> For just <h2>${price}</h2>`);
+// });
 
 //------2------
 app.get('/', (req, res) => {
@@ -22,6 +22,18 @@ app.get('/', (req, res) => {
   const __dirname = path.dirname(__filename);
 
   res.sendFile(path.join(__dirname, './index.html'));
+});
+
+//-----3-----
+app.set('view engine', 'pug');
+
+app.get('/product', (req, res) => {
+  const title = req.query?.title || 'Title';
+  const price = req.query?.price || 'Price';
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  res.render('index', {title, price});
 });
 
 app.listen(PORT, () => console.log('Server is running...'));
